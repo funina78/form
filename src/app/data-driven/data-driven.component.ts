a9165663167b8a713b8a2d9685b04a9585b5e779
nifu <nifu@paypal.com>
2017-02-18 22:20:44 -0800

    data driven form

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
     selector: 'data-driven',
@@ -31,25 +33,59 @@ export class DataDrivenComponent {
 //way 2 : user formBuilder
     this.myForm = formBuilder.group({
         'userData': formBuilder.group({
-            'username': ['Nina', Validators.required],
-            'email': ['',
+            'username': ['Nina', [Validators.required, this.exampleValidator]],
+            'email': [
+                '',
                 Validators.required,
-                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"), ]
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"),
+            ],
 
         }),
         'password': ['', Validators.required],
         'gender': ['male'],
         'hobbies': formBuilder.array([
-            ['Cooking', Validators.required],
+            ['Cooking', Validators.required, this.asyncExampleValidator],
         ])
     });
+
+    this.myForm.valueChanges.subscribe(
+        (data: any) => console.log(data)
+    );
+
+    this.myForm.statusChanges.subscribe(
+        (data: any) => console.log(data)
+    );
  }
 
     onAddHobbie() {
         //cast the type to FormArray
-        (<FormArray>this.myForm.get('hobbies')).push(new FormControl('', Validators.required));
+        (<FormArray>this.myForm.get('hobbies')).push(new FormControl('', Validators.required, this.asyncExampleValidator));
     }
-     onSubmit() {
-         console.log(this.myForm);
-     }
+    onSubmit() {
+        console.log(this.myForm);
+    }
+    // return value is a key value pair, key is a string, value is boolean type
+    exampleValidator(control: FormControl): {[s: string]: boolean} {
+        //return anything means validation fail. In case of validation fail, has to return value (true), can't return value (false)
+        if (control.value === 'Example') {
+            return {example: true};
+        }
+        //return null if validation pass
+        return null;
+    }
+
+    asyncExampleValidator(control: FormControl): Promise<any> | Observable<any> {
+        const promise = new Promise<any>(
+            (resolve, reject) => {
+                setTimeout(() => {
+                    if (control.value === 'Example') {
+                        resolve({'invalid': true}); //resolve and return value true, means validation failed
+                    } else {
+                        resolve(null);
+                    }
+                }, 1500);
+            }
+        );
+        return promise;
+    }
 }
